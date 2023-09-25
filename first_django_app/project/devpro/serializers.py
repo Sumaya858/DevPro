a80a6efc5b91640631e5ccaddbe432f19caf1f67
Sumaya Alyas <elias2sumaya@gmail.com>
2023-09-25 20:13:49 +0300

    models and what relates

--- first_django_app/project/devpro/serializers.py
+++ first_django_app/project/devpro/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Items
 from .models import Whishlist
+from .models import Order
+from .models import Product
+from .models import OrderProduct
 
 
 class ItemsSerializer(serializers.ModelSerializer):
@@ -12,4 +15,22 @@ class ItemsSerializer(serializers.ModelSerializer):
 class WhishlistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Whishlist
-        fields = '__all__'        
+        fields = '__all__'   
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderProduct
+        fields = '__all__'
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'                             
--- first_django_app/project/devpro/urls.py
+++ first_django_app/project/devpro/urls.py
@@ -13,6 +13,27 @@ from .views import WhishlistDeleteView
 from .views import WhishlistUpdateView
 from .views import WhishlistRetrieveView
 
+from .views import OrderProductListCreate
+from .views import OrderProductCreateView
+from .views import OrderProductListView
+from .views import OrderProductDeleteView
+from .views import OrderProductUpdateView
+from .views import OrderProductRetrieveView
+
+from .views import OrderListCreate
+from .views import OrderCreateView
+from .views import OrderListView
+from .views import OrderDeleteView
+from .views import OrderUpdateView
+from .views import OrderRetrieveView
+
+from .views import ProductListCreate
+from .views import ProductCreateView
+from .views import ProductListView
+from .views import ProductDeleteView
+from .views import ProductUpdateView
+from .views import ProductRetrieveView
+
 
 urlpatterns = [
     path('api/items/', ItemsListCreate.as_view()),
@@ -28,5 +49,27 @@ urlpatterns = [
     path('api/whishlist/<pk>/delete', WhishlistDeleteView.as_view()),
     path('api/whishlist/<pk>/update', WhishlistUpdateView.as_view()),
     path('api/whishlist/<pk>/retrieve', WhishlistRetrieveView.as_view()),
+
+
+    path('api/orderproduct/', OrderProductListCreate.as_view()),
+    path('api/orderproduct/create/', OrderProductCreateView()),
+    path('api/orderproduct/list/',OrderProductListView.as_view()),
+    path('api/orderproduct/<pk>/delete', OrderProductDeleteView.as_view()),
+    path('api/orderproduct/<pk>/update', OrderProductUpdateView.as_view()),
+    path('api/orderproduct/<pk>/retrieve', OrderProductRetrieveView.as_view()),
+
+    path('api/order/', OrderListCreate.as_view()),
+    path('api/order/create/', OrderCreateView()),
+    path('api/order/list/',OrderListView.as_view()),
+    path('api/order/<pk>/delete', OrderDeleteView.as_view()),
+    path('api/order/<pk>/update', OrderUpdateView.as_view()),
+    path('api/order/<pk>/retrieve', OrderRetrieveView.as_view()),
+
+    path('api/product/', ProductListCreate.as_view()),
+    path('api/product/create/', ProductCreateView()),
+    path('api/product/list/',ProductListView.as_view()),
+    path('api/product/<pk>/delete', ProductDeleteView.as_view()),
+    path('api/product/<pk>/update', ProductUpdateView.as_view()),
+    path('api/product/<pk>/retrieve', ProductRetrieveView.as_view()),
 
 ]
